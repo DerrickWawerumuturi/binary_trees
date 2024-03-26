94fc9066cdba9e7f3daf749c026731be3ba384a0
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2024-03-26 14:06:35 +0300

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,16 +1,19 @@
 #include "binary_trees.h"
 
-size_t binary_tree_height(const binary_tree_t *tree) {
-    if (tree == NULL) {
-        return (0);
-    }
-    size_t left = binary_tree_height(tree->left);
-    size_t right = binary_tree_height(tree->right);
+/**
+ * binary_tree_height - calculates the height of the binary tree
+ * @tree: binary tree to calculate the height of
+ * Return:  the height on success and 0 on failure
+*/
 
-    if (left > right) {
-        return (left + 1);
+size_t binary_tree_height(const binary_tree_t *tree) {
+    if (tree) {
+        size_t l = 0, r = 0;
+
+
+        l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+        r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+        return ((l > r) ? l : r);
     }
-    else {
-        return (right + 1);
-    }
+    return (0);
 }
