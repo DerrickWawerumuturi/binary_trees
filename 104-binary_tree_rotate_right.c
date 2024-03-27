3b8081f6be61b20e35653f0f9b244151dcec9e49
Derrick Muturi <127112383+DerrickWawerumuturi@users.noreply.github.com>
2024-03-27 19:50:06 +0300

    104

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,25 +1,34 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_right - performs a right-roattion 
- * on a binary tree
- * @tree: a pointer to the root node of the tree to rotate
- * Return: apointer to the noew root once rotated
-*/
+ * binary_tree_rotate_right - Right-rotates a binary tree.
+ * @tree: A pointer to the root node of the tree to rotate.
+ *
+ * Return: A pointer to the new root node after rotation.
+ */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-    binary_tree_t *temp, *nxt;
-    if (tree == NULL || tree->left  == NULL)
-        return (NULL);
+	binary_tree_t *pivot, *tmp;
 
-    temp =  tree->left;
-    nxt = temp->right;
-    temp->right =  tree;
-    tree->left = nxt;
-    if (nxt != NULL)
-        nxt->parent = tree;
+	if (tree == NULL || tree->left == NULL)
+		return (NULL);
 
-    nxt = tree->parent;
-    tree->parent = temp;
-    pivot->
+	pivot = tree->left;
+	tmp = pivot->right;
+	pivot->right = tree;
+	tree->left = tmp;
+	if (tmp != NULL)
+		tmp->parent = tree;
+	tmp = tree->parent;
+	tree->parent = pivot;
+	pivot->parent = tmp;
+	if (tmp != NULL)
+	{
+		if (tmp->left == tree)
+			tmp->left = pivot;
+		else
+			tmp->right = pivot;
+	}
+
+	return (pivot);
 }
